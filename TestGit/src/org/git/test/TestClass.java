e79f07cb3ba4b81730cc09e72c1876d7cd00588c
poojanetra <pooja95.s.sharma@gmail.com>
2020-06-21 13:16:03 +0530

    Updated Test class from Git Ui

--- TestGit/src/org/git/test/TestClass.java
+++ TestGit/src/org/git/test/TestClass.java
@@ -8,6 +8,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is main");
+		System.out.println("Added after from UI");
 	}
 
 }
