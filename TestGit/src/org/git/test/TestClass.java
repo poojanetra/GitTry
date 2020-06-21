6b85fd1316a71f319b3a9a230590bcbe2c34ec04
poojanetra <pooja95.s.sharma@gmail.com>
2020-06-21 13:08:07 +0530

    Adding Test main class

---
+++ TestGit/src/org/git/test/TestClass.java
@@ -0,0 +1,13 @@
+package org.git.test;
+
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is main");
+	}
+
+}
